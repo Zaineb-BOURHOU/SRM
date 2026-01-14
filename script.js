8f6d4ebeaeb989e76c21ffb76a436bcb00808525
ZAINEB BOURHOU <194725065+Zaineb-BOURHOU@users.noreply.github.com>
2026-01-14 17:17:12 +0100

    Refactor image slideshow code by removing comments
    Removed comments and cleaned up the code for the image slideshow functionality.

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-
-  // Tableau avec les images
   const images = [
     "srm.png",
     "img2.jpg",
@@ -10,17 +8,15 @@
   const photo = document.getElementById("photo");
   const dots = document.querySelectorAll("#dots span");
 
-  // Fonction pour afficher un slide
   function showSlide(index) {
     currentIndex = index;
     photo.style.backgroundImage = `url('${images[index]}')`;
 
-    // Gérer les points (dots)
+
     dots.forEach(dot => dot.classList.remove("active"));
     dots[index].classList.add("active");
   }
 
-  // Suivant / Précédent
   function nextSlide() {
     currentIndex = (currentIndex + 1) % images.length;
     showSlide(currentIndex);
